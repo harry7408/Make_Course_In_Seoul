aba9ea2979a2160655e0cec314e2dd165909b1e7
harry <harry7408@naver.com>
2023-03-21 20:34:37 +0900

    회원가입 페이지 UI작성

--- app/src/main/java/com/example/whattodo/MainActivity.kt
+++ app/src/main/java/com/example/whattodo/MainActivity.kt
@@ -1,11 +1,20 @@
 package com.example.whattodo
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+    lateinit var text:TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        text=findViewById(R.id.joinText)
+        text.setOnClickListener {
+            val intent=Intent(this,JoinActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
