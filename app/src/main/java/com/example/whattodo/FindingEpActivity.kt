1e96d2152465b30d83fcd883cdf6efcd7d3d2b54
harry <harry7408@naver.com>
2023-03-23 21:01:39 +0900

    페이지 text 추가사항 + 네비게이션 틀 추가

---
+++ app/src/main/java/com/example/whattodo/FindingEpActivity.kt
@@ -0,0 +1,11 @@
+package com.example.whattodo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class FindingEpActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_finding_ep)
+    }
+}
--- app/src/main/java/com/example/whattodo/MainActivity.kt
+++ app/src/main/java/com/example/whattodo/MainActivity.kt
@@ -3,16 +3,32 @@ package com.example.whattodo
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
 import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
-    lateinit var text:TextView
+    private lateinit var emailedt:EditText
+    private lateinit var passedt:EditText
+    private lateinit var findemailtxt:TextView
+    private lateinit var findpasstxt:TextView
+    private lateinit var jointxt:TextView
+    private lateinit var loginbtn: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        text=findViewById(R.id.joinText)
-        text.setOnClickListener {
+        emailedt=findViewById<EditText>(R.id.emailArea)
+        passedt=findViewById<EditText>(R.id.passArea)
+        findemailtxt=findViewById<TextView>(R.id.findIdText)
+        findpasstxt=findViewById<TextView>(R.id.findPassText)
+        jointxt=findViewById<TextView>(R.id.joinText)
+        loginbtn=findViewById<Button>(R.id.loginButton)
+
+
+
+
+        jointxt.setOnClickListener {
             val intent=Intent(this,JoinActivity::class.java)
             startActivity(intent)
         }
